90e1d6e0d902103d1e06ccfd979a7e276801d1db
boki <lsb530@naver.com>
2021-02-25 22:57:09 +0900

    Start Part4

--- src/main/java/org/zerock/domain/Ticket.java
+++ src/main/java/org/zerock/domain/Ticket.java
@@ -1,8 +1,10 @@
 package org.zerock.domain;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class Ticket {
     private int tno;
     private String owner;
